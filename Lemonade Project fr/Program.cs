84bf8d2db1df53d85ace7237040f1a3d8132628f
Michael Rackowski <MichaelRackowski@gmail.com>
2019-09-04 09:02:17 -0500

    adding new files

---
+++ Lemonade Project fr/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lemonade_Project_fr
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            game game = new game();
+            game.rungame();
+        }
+    }
+}
---
+++ Lemonade Project fr/weather.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lemonade_Project_fr
+{
+    class weather
+    {
+       public int actualTemperature;
+       public int forecastedTemperature;
+        public List<string> conditionOptions;
+        Random random;
+        public string condition;
+
+
+
+ 
+
+        public void tempatureCreator()
+        {
+            forecastedTemperature = random.Next(60, 100);
+            Console.ReadLine();
+        }
+        public void conditionCreator()
+        {
+            conditionOptions = new List<string>() {"Humid", "Cloudy", "Sunny", "Rainy", };
+
+
+
+             Console.ReadLine();
+        }
+
+        public void forecastCreator()
+        {
+            actualTemperature = random.Next(forecastedTemperature - 5, forecastedTemperature + 5); 
+            
+                
+                
+
+
+
+        }
+
+
+    }
+}
+
+
+
+
+
+
+
+
+
